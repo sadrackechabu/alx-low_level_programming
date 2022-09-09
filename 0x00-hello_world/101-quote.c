a4c2537bb3a4072edce37d1bcba798d8c2ec52d0
sadrackechabu <sadrackechabu.1993@gmail.com>
2022-09-08 19:00:43 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <sdtio>
+/**
+ * main - printing and that piece of art is useful Dora Korpar
+ *
+ * Return:1 otherwise 0
+ */
+int  main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
