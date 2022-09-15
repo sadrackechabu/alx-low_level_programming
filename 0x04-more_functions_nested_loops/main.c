b9f3bfd536d7a0f45d2451ebae7b397f4883720d
sadrackechabu <sadrackechabu.1993@gmail.com>
2022-09-15 10:27:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - takes input from
+ * the _putchar function
+ *
+ * Return: On success 1.
+ */
+int main(void)
+{
+	int _putchar(char c);
+	int _isupper(int c);
+	int _isdigit(int c);
+	int mul(int a, int b);
+	void print_numbers(void);
+	void print_most_numbers(void);
+	void more_numbers(void);
+	void print_line(int n);
+	void print_diagonal(int n);
+	void print_square(int size);
+	void print_triangle(int size);
+	return (1);
+}
