560c773573556e04e4ccd0691a574e54940d2b7a
sadrackechabu <sadrackechabu.1993@gmail.com>
2022-09-21 19:15:27 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * main - main entry block
+  * Takes some paramters
+  * Return: nothing
+  */
+int main(void)
+{
+	int _putchar(char c);
+	char *_strcat(char *dest, char *src);
+	char *_strncat(char *dest, char *src, int n);
+	char *_strncpy(char *dest, char *src, int n);
+	int _strcmp(char *s1, char *s2);
+	void reverse_array(int *a, int n);
+	char *string_toupper(char *);
+	char *cap_string(char *);
+	return (0);
+}
+
